7bf4c10a30f5dc022f8a580082f6943a97e47e19
Willian <wsilvadev@gmail.com>
2020-07-15 21:56:04 -0300

    feat(utils): create weather conditions

---
+++ src/utils/WeatherConditions.js
@@ -0,0 +1,56 @@
+export const weatherConditions = {
+  Rain: {
+    color: "#005BEA",
+    title: "Chovendo",
+    subtitle: "Pegue uma xícara de café",
+    icon: "weather-rainy",
+  },
+  ClearDay: {
+    color: "#f7b733",
+    title: "Ensolarado",
+    subtitle: "Está machucando meus olhos",
+    icon: "weather-sunny",
+  },
+  ClearNight: {
+    color: "#29505d",
+    title: "Céu limpo",
+    subtitle: "Uma boa hora para olhar as estrelas",
+    icon: "weather-night",
+  },
+  Thunderstorm: {
+    color: "#616161",
+    title: "Uma tempestade se aproxima",
+    icon: "weather-lightning",
+  },
+  Clouds: {
+    color: "#1F1C2C",
+    title: "Nuvens",
+    subtitle: "Em toda parte",
+    icon: "weather-cloudy",
+  },
+
+  Snow: {
+    color: "#00d2ff",
+    title: "Neve",
+    subtitle: "Saia e construa um boneco de neve para mim",
+    icon: "weather-snowy",
+  },
+  Drizzle: {
+    color: "#076585",
+    title: "Chuvisco",
+    subtitle: "Chovendo parcialmente ...",
+    icon: "weather-hail",
+  },
+  Haze: {
+    color: "#66A6FF",
+    title: "Neblina",
+    subtitle: "Outro nome para chuva parcial",
+    icon: "weather-hail",
+  },
+  Mist: {
+    color: "#3CD3AD",
+    title: "Névoa",
+    subtitle: "Não vagueie pelas florestas!",
+    icon: "weather-fog",
+  },
+};
